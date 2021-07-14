092f62d1c64008bbc54248e11292e20e4f8b7541
Cawolf <camilozambranovotto@hotmail.com>
2021-07-14 00:36:17 -0500

    :rage4: Fiix issues with the concat of buffers

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const audioOptions = {
     timeout: 10000
 }
 
+function testRegex(base64String) {
+    const re = /(\/\/NExKwAAANIAAAAADEwMFV+TEFNRTMu)+/g;
+    const removed = base64String.replace(re, "");
+    return removed;
+}
 
 async function textToUse() {
     let file = "test";
@@ -22,10 +27,12 @@ async function simpleTest() {
     try {
         const texto = await textToUse();
         const base64Results = await googleTTS.getAllAudioBase64(texto, audioOptions);
-        const base64Audio = base64Results.map(result => result.base64).join("");
+        const base64Result = base64Results.map(result => result.base64).join("");
+        const base64Audio = testRegex(base64Result);
         console.log("We got the audio from Google!");
+        await fs.writeFile("./a-revisar4.txt", base64Audio);
         const audioBuffer = Buffer.from(base64Audio, "base64");
-        await fs.writeFile(".audio.mp3", audioBuffer, "base64");
+        await fs.writeFile("./audio4.mp3", audioBuffer, "base64");
         console.log("Audio saved!");
     } catch (error) {
         console.log(error);
